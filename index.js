b70c141a2753409b86011342f5f0f740109b6389
becode <patez.sebastien@gmail.com>
2022-05-23 14:14:23 +0200

    created the first event and added a little structure in html

---
+++ index.js
@@ -0,0 +1,8 @@
+let event = [
+    {
+        name: "Mon concert de hard bop electro punk",
+        dates: ["2022-07-01", "2022-09-23"],
+        author: "Méga Sébastien",
+        description: "akshualy it's just good music dw ^^ UwU"
+    }
+]
