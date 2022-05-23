e640a1ff569a97bbab0543eb6974bda26d92761b
becode <patez.sebastien@gmail.com>
2022-05-23 16:50:13 +0200

    added ambitious stuff

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ let eventList = [
         dates: ["2022-07-01", "2022-09-23"],
         author: "Méga Sébastien",
         description: "akshualy it's just good music dw ^^ UwU"
+    },
+    {
+        name: "je vais cuisiner mes enfants",
+        dates: ["2023-07-01", "2024-09-23"],
+        author: "Marc",
+        description: "tendre et chaud"
     }
 ]
 
@@ -17,27 +23,69 @@ function displayEventAdder()
     document.body.querySelector("#closeModal").addEventListener("click", () =>{modalWindow.style.display = "none"})
 }
 
+// function displayAvailability()
+// {
+
+// }
+
 function displayEvents()
 {
     let Eventscontainer = document.body.querySelector(".EventContainer");
-
+    let i = 0;
     for (const event of eventList) {
         let eventDiv = document.createElement("div");
         let eventName = document.createElement("div");
-        let eventAuthor = document.createElement("div");
         let eventDates = document.createElement("div");
         let eventDescription = document.createElement("div");
+
         eventDiv.setAttribute("class", "eventElement")
+        eventDescription.setAttribute("class", "eventDescription")
+        eventDates.setAttribute("class", "eventDates")
+
         Eventscontainer.appendChild(eventDiv);
         eventDiv.appendChild(eventName);
-        eventDiv.appendChild(eventAuthor);
-        eventDiv.appendChild(eventDates);
         eventDiv.appendChild(eventDescription);
+        eventDiv.appendChild(eventDates);
         eventName.innerHTML = `${event.author}'s ${event.name}`;
         eventDescription.innerHTML = event.description;
-        eventDates.innerHTML = event.dates;
-        // eventAuthor.innerHTML = event.author;
         
+        for (const date of event.dates) {
+            let br = document.createElement("br")
+            let dateValue = document.createElement("p")
+            let dateHolder = document.createElement("div");
+            dateHolder.setAttribute("class", "dateHolder")
+            let checkYes = document.createElement("input");
+            let checkNo = document.createElement("input");
+            let labelYes = document.createElement('label');
+            let labelNo = document.createElement('label');
+            
+            checkNo.type = 'checkbox';
+            checkNo.id = 'NoBox';
+            checkNo.name = 'interest';
+            checkNo.value = 'No';
+            labelNo.htmlFor = 'No';
+            
+            checkYes.type = 'checkbox';
+            checkYes.id = 'YesBox';
+            checkYes.name = 'interest';
+            checkYes.value = 'Yes';
+            labelYes.htmlFor = 'Yes';
+            dateValue.innerHTML = date;
+            dateValue.setAttribute("class", `event${i}`)
+            dateHolder.appendChild(dateValue)
+            dateHolder.appendChild(br);
+            labelYes.appendChild(document.createTextNode('Yes'));
+            labelNo.appendChild(document.createTextNode('No'));
+            eventDates.appendChild(dateHolder);
+            dateHolder.appendChild(checkYes);
+            dateHolder.appendChild(labelYes)
+            dateHolder.appendChild(checkNo);
+            dateHolder.appendChild(labelNo)
+            
+            // dateValue.addEventListener("mouseover", () =>{displayAvailability(i)})
+            // is supposed to display a window with people who are available this day on hover
+        }
+        i++;
     }
 }
 
