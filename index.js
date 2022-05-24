fb380078512240bb83a2fe784655f843b370676e
becode <patez.sebastien@gmail.com>
2022-05-24 12:33:16 +0200

    added some appearance features

--- index.js
+++ index.js
@@ -11,10 +11,24 @@ function displayEventAdder()
     document.body.querySelector("#closeModal").addEventListener("click", () =>{modalWindow.style.display = "none"})
 }
 
-// function displayAvailability()
-// {
+function displayAvailability()
+{
+    console.log("has to display availability in a little philactère")
+}
 
-// }
+function deleteEvent()
+{
+    console.log("has to delete the event")
+    let modalWindow = document.createElement("div");
+    modalWindow.setAttribute("class", "modalWindow");
+    document.body.appendChild(modalWindow);
+    modalWindow.style.display = "block";
+}
+
+function editEvent()
+{
+    console.log("has to edit the event")
+}
 
 function displayEvents()
 {
@@ -56,12 +70,15 @@ function displayEvents()
             checkNo.name = 'interest';
             checkNo.value = 'No';
             labelNo.htmlFor = 'No';
+            checkNo.setAttribute("class", 'radio')
             
             checkYes.type = 'checkbox';
             checkYes.id = 'YesBox';
             checkYes.name = 'interest';
             checkYes.value = 'Yes';
             labelYes.htmlFor = 'Yes';
+            checkYes.setAttribute("class", 'radio')
+
             dateValue.innerHTML = date.date;
             dateValue.setAttribute("class", `event${i}`)
             dateHolder.appendChild(dateValue)
@@ -74,10 +91,22 @@ function displayEvents()
             dateHolder.appendChild(checkNo);
             dateHolder.appendChild(labelNo)
             
-            // dateValue.addEventListener("mouseover", () =>{displayAvailability(i)})
+            dateValue.addEventListener("mouseover", () =>{displayAvailability()})
             // is supposed to display a window with people who are available this day on hover
         }
         i++;
+        let deleteBtn = document.createElement("div");
+        let editBtn = document.createElement("div");
+        let btns = document.createElement("div");
+        btns.setAttribute("class", "delAndEdit");
+        deleteBtn.innerHTML = "Delete";
+        editBtn.innerHTML = "Edit";
+        eventDiv.appendChild(btns);
+        btns.appendChild(editBtn);
+        btns.appendChild(deleteBtn);
+        deleteBtn.addEventListener("click", deleteEvent)
+        editBtn.addEventListener("click", editEvent)
+
     }
 }
 
@@ -120,7 +149,10 @@ function addEvent()
     newEvent.name = document.querySelector("#nameInput").value;
     newEvent.description = document.querySelector("#descriptionInput").value;
     newEvent.dates = [];
-    newEvent.dates.push(document.querySelector("#dateInput").value)
+    for (let elem of document.querySelectorAll(".dateInput")) {
+        
+        newEvent.dates.push(elem.value)
+    }
     newEvent.author = User;
     console.log(newEvent);
     console.log(eventList);
@@ -129,7 +161,26 @@ function addEvent()
 }
 
 
+function displayAnotherDateInput()
+{
+    let inputs = document.body.querySelectorAll(".dateInput")
+    if(!inputs[inputs.length -1].value)
+        return;
+    let container = document.querySelector(".dateInputCtn");
+    let br = document.createElement("br");
+    let newDate = document.createElement("input");
+    newDate.type = "text";
+    newDate.setAttribute("class", "dateInput");
+    container.appendChild(newDate);
+    container.appendChild(br);
+
+    container.insertBefore(document.body.querySelector(".addDate"), br.nextSibling)
+    newDate.style.marginBottom = "5px";
+    
+}
+
 fetchEventList()
 document.body.querySelector("select").addEventListener("change", ()=>User = document.querySelector("select").value)
 document.body.querySelector("#addEvent").addEventListener("click", displayEventAdder)
 document.body.querySelector("#submitEvent").addEventListener("click", addEvent)
+document.body.querySelector(".addDate").addEventListener("click", displayAnotherDateInput)
